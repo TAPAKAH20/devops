0aec56aa9b28c8965178debc8d5caa8c6621c0a7
R0ach <tapakah20@ya.ru>
2021-09-26 20:04:01 +0300

    Lab 12

--- python_app/main.py
+++ python_app/main.py
@@ -19,6 +19,8 @@ def index():
         Takes current time and displays it on the "/" page
     """
     time_now = datetime.datetime.now(tz=MSK_TZ)
+    with open("visitors.txt", "a") as f:
+        f.write(time_now.strftime('%X') + '\n')
     return f"<p> {time_now.strftime('%X')} </p>"
 
 @APP.route("/healthcheck")
@@ -27,3 +29,11 @@ def health_check():
         Stub for future healthcheck function
     """
     return ''
+
+@APP.route("/visitors")
+def visitors():
+    resp = ''
+    with open("visitors.txt") as f:
+        for line in f.readlines():
+            resp += f"<p> {line} </p>"
+    return resp
